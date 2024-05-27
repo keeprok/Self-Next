b119edf730f3329ab50cadd813cd543043b80781
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-27 23:58:50 +0900

    feat  : nomad next 4완

--- Task6/learn-nextjs14/app/(home)/page.tsx
+++ Task6/learn-nextjs14/app/(home)/page.tsx
@@ -1,10 +1,23 @@
+import Movie from '../../components/movie';
+import styles from '../../styles/home.module.css';
 export const metadata = {
   title: 'Home',
 };
-export default function Tomato() {
+
+export const API_URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const response = await fetch(API_URL);
+  const json = await response.json();
+  return json;
+}
+export default async function Tomato() {
+  const movies = await getMovies();
   return (
-    <div>
-      <h1>Hello Nextjs!</h1>
+    <div className={styles.container}>
+      {movies.map((movie) => (
+        <Movie key={movie.id} id={movie.id} poster_path={movie.poster_path} title={movie.title} />
+      ))}
     </div>
   );
 }
